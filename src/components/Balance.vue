6f48a0512a5bbfa0a914c55bd66230371c899c4f
JDRXcr <jj.duartem@gmail.com>
2024-06-25 13:32:16 +0200

    money-tracker

---
+++ src/components/Balance.vue
@@ -0,0 +1,17 @@
+<template>
+  <fragment>
+    <h4>Your Balance</h4>
+    <h1 id="balance">${{ total }}</h1>
+  </fragment>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
